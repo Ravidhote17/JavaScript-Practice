28e1b492f76814a095edb8a4874da6b07f1f2e40
Ravidhote17 <ravidhote101@gmail.com>
2024-05-02 19:03:41 +0530

    first commit

---
+++ consoleAlertPrompt.js
@@ -0,0 +1,17 @@
+//CONSOLE
+
+//console.log
+console.log("Hello World")
+
+//console.warn
+console.warn("Warninig")
+
+//console.error
+console.log("Error...")
+
+
+//ALERT
+alert("Hey")
+
+//PROMPT
+prompt("Enter the age: ")
---
+++ varConstLet.js
@@ -0,0 +1,20 @@
+// VARIABLE
+var a = 574;
+var b = 98;
+
+var sum = a+b; 
+console.log(sum);
+
+// CONSTANT
+const c = 5841;
+const d = 641;
+
+const multy = a*b
+console.log(multy)
+
+//LET
+let e = 685;
+let f = 365;
+
+let div = e/f;
+console.log(div)
